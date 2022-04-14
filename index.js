6a2bc4c0dc8a68edb19ab417df82696d8f054f05
danielNishimura <nishimurani@gmail.com>
2022-04-13 23:19:37 -0300

    comentarios e strings e numeros

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const name = "Daniel Nishimura"
+//Criar um programa que calcula a media
+// das notas entre os alunos e envia
+// mensagem do calculo da média
 
-console.log(name)
+const name = "Daniel Nishimura"
+const nome2 = `Daniel e ${name}`
+console.log(nome2)
